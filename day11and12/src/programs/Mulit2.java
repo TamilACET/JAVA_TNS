d2b49c6a83335024ddcce864fbd8c450f5ad9836
TNS INDIA <TNS INDIA@LAPTOP-JIMRE4K7>
2023-02-23 15:32:47 +0530

    i added one more line in multi2 program

--- day11and12/src/programs/Mulit2.java
+++ day11and12/src/programs/Mulit2.java
@@ -5,6 +5,7 @@ public class Mulit2 implements Runnable{
 		public void run()
 		{
 			System.out.println("thread is running");
+			System.out.println("Adding the line");
 		}
 		public static void main(String[] args) {
 			Mulit2 m1=new Mulit2();
